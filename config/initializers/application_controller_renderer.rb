a64da6c0bfa1c5742c53ad89ed475a110addd6e6
EC2 Default User <ec2-user@ip-172-31-37-242.ec2.internal>
2021-05-05 07:51:37 +0000

    [finish]2-7 画像の追加機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '70b31f8d4a8a63d747b494a9dc4eb22422fbe569c2e60cb538ccba4b9028da85ba0660cffcd78e72cc4b44a19960860f465bc95933e671482c8d1952a7e56f9a'
